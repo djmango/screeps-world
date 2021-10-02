541c22fd513358ae9c2bbfc22d140ef9c44bcec0
Sulaiman Ghori <skghori03@gmail.com>
2021-10-02 17:27:26 +0200

    added utils

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const utils = require('utils');
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
@@ -7,26 +8,26 @@ var roleRepairer = require('role.repairer');
 
 // out of (sum of object) creeps, how many do you want to be the specified role?
 const jobDistribution = {
-    'harvester': 2,
-    'upgrader': 5,
+    'harvester': 5,
+    'upgrader': 2,
     'builder': 2,
-    'repairer': 2
+    'repairer': 1
 }
 const jobs = Object.keys(jobDistribution)
 
 // helper functions, should move this to a utils at some point
-const randomProperty = function (obj) {
-    var keys = Object.keys(obj);
-    return obj[keys[ keys.length * Math.random() << 0]];
-};
+// const randomProperty = function (obj) {
+//     var keys = Object.keys(obj);
+//     return obj[keys[ keys.length * Math.random() << 0]];
+// };
 
-const sumObjectValues = obj => Object.values(obj).reduce((a, b) => a + b);
+// const sumObjectValues = obj => Object.values(obj).reduce((a, b) => a + b);
 
 function calculateJobDist(job) {
     // check that the job provided is valid
     if (!jobDistribution.hasOwnProperty(job)) return console.log(`ERROR: ${job} is not a a valid job!`)
 
-    total = sumObjectValues(jobDistribution)
+    total = utils.sumObjectValues(jobDistribution)
 
     desiredRatio = jobDistribution[job] / total
     return desiredRatio
@@ -35,7 +36,7 @@ function calculateJobDist(job) {
 module.exports.loop = function () {
     // housekeeping
 
-    // clear out dead creeps
+    // clear out dead creeps memory
     for (var name in Memory.creeps) {
         if (!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -43,7 +44,7 @@ module.exports.loop = function () {
         }
     }
 
-    // validate creep memory
+    // ensure valid creep memory
     for (const i in Game.creeps) {
         let creep = Game.creeps[i]
         if (!creep.memory.hasOwnProperty('lastReassignment')) {
@@ -54,16 +55,18 @@ module.exports.loop = function () {
         }
     }
 
+    // our total creeps :D
     var totalCreeps = Object.keys(Game.creeps).length
     console.log(`Total creeps (by radiohead): ${totalCreeps}`)
 
+    // job redistro and reassig
     jobs.forEach((job) => {
         let creepsInJob = _.filter(Game.creeps, (creep) => creep.memory.job == job);
         console.log(`Total ${job}s: ${creepsInJob.length}`)
 
         // check if we are below ratio according to defined distrobution
         if ((creepsInJob.length / totalCreeps) < calculateJobDist(job)) {
-            randomCreep = randomProperty(_.filter(Game.creeps, (creep) => (creep.memory.job != job && (Game.time - creep.memory.lastReassignment) > 50)))
+            randomCreep = utils.randomProperty(_.filter(Game.creeps, (creep) => (creep.memory.job != job && (Game.time - creep.memory.lastReassignment) > 50)))
             if (randomCreep !== undefined) { // we wont always be able to reassign
                 randomCreep.memory.job = job
                 randomCreep.memory.lastReassignment = Game.time
@@ -72,12 +75,13 @@ module.exports.loop = function () {
         }
     })
 
+    // spawn if we are close to capacity
     if (Game.spawns['Home'].store.getFreeCapacity(RESOURCE_ENERGY) < 50) {
         var newName = 'Radiohead' + Game.time;
         console.log('Spawning new creep: ' + newName);
         Game.spawns['Home'].spawnCreep([WORK, CARRY, MOVE], newName, {
             memory: {
-                job: 'harvester'
+                job: 'okcomputer'
             }
         });
     }
@@ -95,17 +99,9 @@ module.exports.loop = function () {
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-        if (creep.memory.job == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if (creep.memory.job == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if (creep.memory.job == 'builder') {
-            roleBuilder.run(creep);
-        }
-        if (creep.memory.job == 'repairer') {
-            roleRepairer.run(creep);
-        }
+        if (creep.memory.job == 'harvester') roleHarvester.run(creep);
+        else if (creep.memory.job == 'upgrader') roleUpgrader.run(creep);
+        else if (creep.memory.job == 'builder') roleBuilder.run(creep);
+        else if (creep.memory.job == 'repairer') roleRepairer.run(creep);
     }
 }
---
+++ utils.js
@@ -0,0 +1,19 @@
+
+const getRandomIntInclusive = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+const randomProperty = function (obj) {
+    var keys = Object.keys(obj);
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
+
+const sumObjectValues = obj => Object.values(obj).reduce((a, b) => a + b);
+
+module.exports = {
+    getRandomIntInclusive,
+    randomProperty,
+    sumObjectValues
+}
